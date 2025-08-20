cd5dd882fdfae172f6a2661427f25c199e476b86
Melese <getawmelese70@gmail.com>
2025-08-20 17:08:37 +0300

    Melese Getaw  - Todo Test Task

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,18 +5,19 @@ import '../models/todo.dart';
 class ApiService {
   static const String baseUrl = 'https://jsonplaceholder.typicode.com';
 
-  // TODO: Implement this function to fetch todos from the API
-  // Use the endpoint: https://jsonplaceholder.typicode.com/todos
-  // Parse the JSON response and return a List<Todo>
-  // Handle any potential errors and throw appropriate exceptions
   Future<List<Todo>> fetchTodos() async {
-    // TODO: Add implementation here
-    // 1. Make HTTP GET request to $baseUrl/todos
-    // 2. Parse the JSON response
-    // 3. Convert each JSON object to Todo using Todo.fromJson
-    // 4. Return the list of todos
-    // 5. Handle errors appropriately
-    
-    throw UnimplementedError('fetchTodos() method needs to be implemented');
+    try {
+      final response = await http.get(Uri.parse('$baseUrl/todos'));
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+
+        return data.map((json) => Todo.fromJson(json)).toList();
+      } else {
+        throw Exception('Failed to load todos: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching todos: $e');
+    }
   }
-} 
+}
