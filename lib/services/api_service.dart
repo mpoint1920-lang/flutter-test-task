a920a97d4733e8987bdd2ef516a3fec9308e77ac
TitusKI <kidumazie@gmail.com>
2025-08-22 21:23:45 +0300

    Implement fetchTodos() method to retrieve and parse todos from the API

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -10,13 +10,18 @@ class ApiService {
   // Parse the JSON response and return a List<Todo>
   // Handle any potential errors and throw appropriate exceptions
   Future<List<Todo>> fetchTodos() async {
-    // TODO: Add implementation here
     // 1. Make HTTP GET request to $baseUrl/todos
-    // 2. Parse the JSON response
-    // 3. Convert each JSON object to Todo using Todo.fromJson
-    // 4. Return the list of todos
+    final response = await http.get(Uri.parse('$baseUrl/todos'));
+
     // 5. Handle errors appropriately
-    
-    throw UnimplementedError('fetchTodos() method needs to be implemented');
+    if (response.statusCode == 200) {
+      // 2. Parse the JSON response
+      final List<dynamic> data = json.decode(response.body);
+      // 3. Convert each JSON object to Todo using Todo.fromJson
+      // 4. Return the list of todos
+      return data.map((json) => Todo.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load todos');
+    }
   }
-} 
+}
